be7a22c757e99f50549b34c5ac697761c909a2d0
RamenHoang <anhnguyenhoang321@gmail.com>
2025-03-07 16:00:30 +0700

    Update get api url from env

--- src/apis/axiosAuthClient.js
+++ src/apis/axiosAuthClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import getCookie from "../utils/getCookie";
 
 const axiosAuthClient = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5500/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: getCookie("authUserToken"),
