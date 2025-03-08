4bdc0bf8c6a08403231cbbef8870bc9a2f9ca32f
RamenHoang <anhnguyenhoang321@gmail.com>
2025-03-08 22:46:07 +0700

    Update to load environment variable

--- src/apis/axiosAuthClient.js
+++ src/apis/axiosAuthClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import getCookie from "../utils/getCookie";
 
 const axiosAuthClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5500/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: getCookie("authUserToken"),
