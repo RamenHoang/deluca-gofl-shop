84644083fbc82da0d7f116653bf25329e4f2d8fe
RamenHoang <anhnguyenhoang321@gmail.com>
2025-03-21 22:54:15 +0700

    Update loader

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,7 +5,12 @@ import './Loading.css';
 const Loading = () => {
     return (
        <div className="loading-component">
-            <BounceLoader size={48} loading css={{ top: '32%', left: '48%' }} />
+            <BounceLoader size={48} loading cssOverride={{ 
+                position: 'fixed',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+             }} />
        </div>
     )
 }
